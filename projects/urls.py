28ed51c915622ad0f9b25f405b1806f52aa82a96
Harshney <harshneyvijay@gmail.com>
2026-07-17 21:24:59 +0530

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -10,15 +10,7 @@ from .views import (
     blogs,
 )
 
-from django.contrib import admin
-from django.urls import path
-
-from projects.views import create_admin
-
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("create-admin/", create_admin),
-    
     path(
         "",
         home,
